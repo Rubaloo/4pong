ea8b8bdafdd2c3b7b173f1c47aefdbd424c7d37c
Ruben Salvador <ru.bcn90@gmail.com>
2017-03-31 00:39:39 +0200

    First socket io connection

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var app = express();
- 
+var app = require('express')();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){ 
+  console.log('Server: client connected');
+  client.on('disconnect', function(){
+    console.log('Server: client disconected');
+  });
+
+  client.emit('event', { id : 'client connected: ' + client.id });
+});
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/src/init.html');
 });
  
-app.listen(3000);
+server.listen(4000);
