258044648decc5bf1fb38ccfee7d17bce356a3d0
Ruben Salvador <ru.bcn90@gmail.com>
2017-04-02 12:17:51 +0200

    Lost refactoring code added

--- src/client.js
+++ src/client.js
@@ -48,5 +48,13 @@ function sendInput(input) {
 
 function drawBall(ball) 
 {
-
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, 10, 2 * Math.PI, false);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.strokeStyle = '#003300';
+    ctx.lineWidth = 1;
+    ctx.stroke();
 }
--- src/game_server.js
+++ src/game_server.js
@@ -1,1 +1,6 @@
 /**The logic for the game server (‘lobby’).*/
+function initGame() {
+  ball = {x : 300, y : 300}; 
+  game = {'ball' : ball};
+  broadcastToClients('initGame', game);
+}
